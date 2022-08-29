207b455eb52c71eb4f3df59735ed545303566907
Komal <waghmarekomal21@gmail.com>
2022-08-29 22:28:47 +0530

    [Komal]Line Comparison

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Day3LineComparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to line comparison");
+            Console.WriteLine("Enter Number\n 1.Length of Line\n 2.Equality of Line\n 3.Compare the length");
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    LengthOfLine.CalculateLengthOfLine();
+                    break;
+                case 2:
+                    EqualityOfLines.CalculateLength();
+                    break;
+                case 3:
+                    Compare2Line.CompareLength();
+                    break;
+                default:
+                    Console.WriteLine("Entered Wrong Number");
+                    break;
+            }
+        }
+
+
+        }
+}
